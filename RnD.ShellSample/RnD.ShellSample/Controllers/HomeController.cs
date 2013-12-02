99c394356c096cdca41af4bc4514ce809b0e64df
raselahmmed <raselahmmed@hotmail.com>
2013-12-02 03:00:46 -0800

    2013-12-01

---
+++ RnD.ShellSample/RnD.ShellSample/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RnD.ShellSample.ViewModels;
+
+namespace RnD.ShellSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public JsonResult GetData()
+        {
+            var shellRewardsPromotionViewModels = new List<ShellRewardsPromotionViewModel>
+                               {
+                                   new ShellRewardsPromotionViewModel{PriorityId = 1, Priority = "ABC", PromoType = "A-TYPE", OfferId = 1, Name = "ABC-NAME", Description = "ABC-DES", StartDate = DateTime.Now, EndDate = DateTime.Now, Status = 1},
+                                   new ShellRewardsPromotionViewModel{PriorityId = 2, Priority = "ABC", PromoType = "A-TYPE", OfferId = 1, Name = "ABC-NAME", Description = "ABC-DES", StartDate = DateTime.Now, EndDate = DateTime.Now, Status = 1},
+                                   new ShellRewardsPromotionViewModel{PriorityId = 3, Priority = "ABC", PromoType = "A-TYPE", OfferId = 1, Name = "ABC-NAME", Description = "ABC-DES", StartDate = DateTime.Now, EndDate = DateTime.Now, Status = 1},
+                                   new ShellRewardsPromotionViewModel{PriorityId = 4, Priority = "ABC", PromoType = "A-TYPE", OfferId = 1, Name = "ABC-NAME", Description = "ABC-DES", StartDate = DateTime.Now, EndDate = DateTime.Now, Status = 1},
+                                   new ShellRewardsPromotionViewModel{PriorityId = 5, Priority = "ABC", PromoType = "A-TYPE", OfferId = 1, Name = "ABC-NAME", Description = "ABC-DES", StartDate = DateTime.Now, EndDate = DateTime.Now, Status = 1},
+                                   new ShellRewardsPromotionViewModel{PriorityId = 6, Priority = "ABC", PromoType = "A-TYPE", OfferId = 1, Name = "ABC-NAME", Description = "ABC-DES", StartDate = DateTime.Now, EndDate = DateTime.Now, Status = 1},
+                                   new ShellRewardsPromotionViewModel{PriorityId = 7, Priority = "ABC", PromoType = "A-TYPE", OfferId = 1, Name = "ABC-NAME", Description = "ABC-DES", StartDate = DateTime.Now, EndDate = DateTime.Now, Status = 1}
+                               };
+
+            return Json(shellRewardsPromotionViewModels, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ RnD.ShellSample/RnD.ShellSample/ViewModels/ShellRewardsPromotionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RnD.ShellSample.ViewModels
+{
+    public class ShellRewardsPromotionViewModel
+    {
+        public int PriorityId { get; set; }
+        public string Priority { get; set; }
+        public string PromoType { get; set; }
+        public int OfferId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Status { get; set; }
+    }
+}
