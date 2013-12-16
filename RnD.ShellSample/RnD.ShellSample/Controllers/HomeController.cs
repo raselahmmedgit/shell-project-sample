3d00de15cafb977173d8e7d00a35f165b7cf0829
raselahmmed <raselahmmed@hotmail.com>
2013-12-15 21:04:21 -0800

    2013-12-15

--- RnD.ShellSample/RnD.ShellSample/Controllers/HomeController.cs
+++ RnD.ShellSample/RnD.ShellSample/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RnD.ShellSample.Models;
 using RnD.ShellSample.ViewModels;
 
 namespace RnD.ShellSample.Controllers
@@ -35,6 +36,69 @@ namespace RnD.ShellSample.Controllers
         public ActionResult TreeView()
         {
             return View();
+        }
+
+        //[HttpPost]
+        public ActionResult GetTreeData(string root)
+        //public ActionResult GetTreeData()
+        {
+            //string root = string.Empty;
+            var dataList = new List<TreeViewModel>()
+                            {
+                              new TreeViewModel  {text = "A", id = "1"},
+                              new TreeViewModel  {text = "B", id = "2"},
+                              new TreeViewModel  {text = "C", id = "3"},
+                              new TreeViewModel  {text = "E", id = "4"},
+                              new TreeViewModel  {text = "F", id = "5"},
+                              new TreeViewModel  {text = "G", id = "6"},
+                              new TreeViewModel  {text = "H", id = "7"},
+                              new TreeViewModel  {text = "I", id = "8"},
+                              new TreeViewModel  {text = "J", id = "9"},
+                              new TreeViewModel  {text = "K", id = "10"},
+
+                              new TreeViewModel  {text = "L", id = "11"},
+                              new TreeViewModel  {text = "M", id = "12"},
+                              new TreeViewModel  {text = "N", id = "13"},
+                              new TreeViewModel  {text = "O", id = "14"},
+                              new TreeViewModel  {text = "P", id = "15"},
+                              new TreeViewModel  {text = "Q", id = "16"},
+                              new TreeViewModel  {text = "S", id = "17"},
+                              new TreeViewModel  {text = "T", id = "18"},
+                              new TreeViewModel  {text = "U", id = "19"},
+                              new TreeViewModel  {text = "V", id = "20"}
+
+                            };
+
+            var nodes = new List<TreeViewModel>();
+
+            if (root == "source")
+            {
+                var partTypes = dataList.ToList().OrderBy(pt => pt.text);
+
+                foreach (var partType in partTypes)
+                {
+                    nodes.Add(new TreeViewModel
+                    {
+                        text = partType.text,
+                        id = partType.id,
+                        hasChildren = true
+                    });
+                }
+            }
+            else
+            {
+                var parts = dataList.Where(p => p.text == root).ToList();
+                foreach (var part in parts)
+                {
+                    nodes.Add(new TreeViewModel
+                    {
+                        text = part.text,
+                        id = part.id
+                    });
+                }
+            }
+
+            return Json(nodes);
         }
 
     }
---
+++ RnD.ShellSample/RnD.ShellSample/Models/TreeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RnD.ShellSample.Models
+{
+    public class TreeViewModel
+    {
+        public TreeViewModel[] children { get; set; }
+
+        public string classes { get; set; }
+
+        public bool expanded { get; set; }
+
+        public bool hasChildren { get; set; }
+
+        public string id { get; set; }
+
+        public string text { get; set; }
+    }
+}
